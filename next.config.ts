eb7e881af6eae86e8e5d4f8210dcc13ccd154e65
Loong Loong <longsizhuo@gmail.com>
2025-04-06 02:07:33 +1100

    bug fixed, it can be run on the net and local

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
+
 const nextConfig: NextConfig = {
-  /* config options here */
+    basePath: isProd ? "/walka" : "",
+    assetPrefix: isProd ? "/walka": "",
     eslint: {
     ignoreDuringBuilds: true,
     }
